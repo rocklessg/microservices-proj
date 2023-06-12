64da48e5265e8d98f6a47b1d60609a08523c3a54
Abdulhafiz <adanmaigoro@fsdhgroup.com>
2023-06-12 17:50:33 +0100

    Implement Discount Protobuf filefor exposing CRUD Services

---
+++ src/Services/Discount/Discount.Grpc.Core/Repositories/Abstractions/IDiscountRepository.cs
@@ -0,0 +1,12 @@
+﻿using Discount.Grpc.Model.Entities;
+
+namespace Discount.Grpc.Core.Repositories.Abstractions
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
