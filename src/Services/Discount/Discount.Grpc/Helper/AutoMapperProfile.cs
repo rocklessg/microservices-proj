986384506103e7342c118cfcce9084b127828850
Abdulhafiz <adanmaigoro@fsdhgroup.com>
2023-06-12 21:28:18 +0100

    Implement gRPC Proto service and Automapper initialization

---
+++ src/Services/Discount/Discount.Grpc/Helper/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Discount.Grpc.Model.Entities;
+using Discount.Grpc.Protos;
+
+namespace Discount.Grpc.Helper
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Coupon, CouponModel>().ReverseMap();
+        }
+    }
+}
--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,5 +1,7 @@
 using Discount.Grpc.Core.Repositories;
 using Discount.Grpc.Core.Repositories.Abstractions;
+using Discount.Grpc.Helper;
+using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,13 +10,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
 builder.Services.AddGrpc();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-//app.MapGrpcService<GreeterService>();
+app.MapGrpcService<DiscountService>();
 
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
